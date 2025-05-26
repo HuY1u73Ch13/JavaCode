38f1f0b03cf7131912d5393e63dd3cf431dcdae9
Nguyễn Huy Hoàng <jokerhuylu@gmail.com>
2025-05-26 07:21:03 +0700

    Update ManageEastAsiaCountries.java

--- 0052-Countr2/src/ManageEastAsiaCountries.java
+++ 0052-Countr2/src/ManageEastAsiaCountries.java
@@ -1,51 +1,56 @@
 
+import com.sun.org.apache.bcel.internal.classfile.Code;
 import java.util.*;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Admin
  */
 public class ManageEastAsiaCountries {
-    
+
     ArrayList<EastAsiaCountries> countryList = new ArrayList<>();
-    Utility u = new Utility();
-    protected  void inputCountry() {
-        if(countryList.size()==11){
-            System.out.println("Enough country !!");
-            return;
-        }
-        String code, name , terrain;
+    Utility utility = new Utility();
+
+    void mockData() {
+        countryList.add(new EastAsiaCountries("VN", "Viet Nam", 4564564, "Nice"));
+        countryList.add(new EastAsiaCountries("TL", "Thai Lan", 4564564, "Nice"));
+    }
+
+    protected void inputCountry() {
+        String code, name, terrain;
         float area;
-        while (true) {            
-            code = u.getString("Enter code of country: ", "Wrong", u.REGEX_CODE);
-            if(checkDuplicateCode(code)){
+        if (countryList.size() == 11) {
+            System.out.println("Enough country !!!");
+        }
+        while (true) {
+            code = utility.getString("Enter code of country: ", "Wrong", utility.REGEX_CODE);
+            if (checkDuplicateCode(code) == true) {
                 System.out.println("DuplicateCode !!!");
-            }else{
+            } else {
                 break;
             }
         }
-        while (true) {            
-            name = u.getString("Enter name of country: ", "Wrong", u.REGEX_NAME);
-            if(checkDuplicateName(name)){
+        while (true) {
+            name = utility.getString("Enter code of country: ", "Wrong", utility.REGEX_CODE);
+            if (checkDuplicateName(name) == true) {
                 System.out.println("DuplicateName !!!");
-            }else{
+            } else {
                 break;
             }
         }
-        area = u.getFloat("Enter total Area: ", "Wrong", 0, Float.MAX_VALUE);
-        terrain = u.getString("Enter terrain of country: ", "Wrong", u.REGEX_NAME);
-        EastAsiaCountries country = new EastAsiaCountries(terrain, code, name, area);
+        area = utility.getFloat("Enter total Area: ", "Wrong", 0, Float.MAX_VALUE);
+        terrain = utility.getString("Enter terrain of country: ", "Wrong", utility.REGEX_NAME);
+        EastAsiaCountries country = new EastAsiaCountries(code, name, area, terrain);
         countryList.add(country);
-    }   
+    }
 
     private boolean checkDuplicateCode(String code) {
-        for(EastAsiaCountries asiaCountry : countryList){
-            if(asiaCountry.getCountryCode().equalsIgnoreCase(code)){
+        for (EastAsiaCountries asiaCountries : countryList) {
+            if (asiaCountries.getCountryCode().equalsIgnoreCase(code)) {
                 return true;
             }
         }
@@ -53,8 +58,8 @@ public class ManageEastAsiaCountries {
     }
 
     private boolean checkDuplicateName(String name) {
-        for(EastAsiaCountries asiaCountry : countryList){
-            if(asiaCountry.getCountryName().equalsIgnoreCase(name)){
+        for (EastAsiaCountries asiaCountries : countryList) {
+            if (asiaCountries.getCountryName().equalsIgnoreCase(name)) {
                 return true;
             }
         }
@@ -62,42 +67,52 @@ public class ManageEastAsiaCountries {
     }
 
     void displayCountryJustInput() {
-        if(countryList.isEmpty()){
-            System.out.println("No country in list !!!");
+        if (countryList.isEmpty()) {
+            System.out.println("No Country in the list !!!");
             return;
         }
         System.out.printf("%-10s %-15s %-10s %-10s\n", "ID", "Name", "Area", "Terrain");
         countryList.get(countryList.size() - 1).display();
     }
-    void searchCountryByName(){
-        String name = u.getString("Enter name: ","Wrong",u.REGEX_NAME).toUpperCase();
+
+    void searchCountryByName() {
+        String name = utility.getString("Enter code of country: ", "Wrong", utility.REGEX_CODE).toUpperCase();
         ArrayList<EastAsiaCountries> searchList = new ArrayList<>();
-        for(EastAsiaCountries asiaCountry : countryList){
-            if(asiaCountry.getCountryName().toUpperCase().contains(name)){
-                searchList.add(asiaCountry);
+
+        for (EastAsiaCountries asiaCountries : countryList) {
+            if (asiaCountries.getCountryName().toUpperCase().contains(name)) {
+                searchList.add(asiaCountries);
             }
         }
-        if(searchList.isEmpty()){
-            System.out.println("NOT FOUND !!");
-        }else {
+
+        if (searchList.isEmpty()) {
+            System.out.println("No Country in the list !!!");
+            return;
+        } else {
             System.out.printf("%-10s %-15s %-10s %-10s\n", "ID", "Name", "Area", "Terrain");
-            for(EastAsiaCountries asiaCountry : countryList){
-                asiaCountry.display();
+            for (EastAsiaCountries asiaCountries : searchList) {
+                asiaCountries.display();
             }
         }
     }
 
-    /*void sortCountry() {
-        if(countryList.isEmpty()){
-            System.out.println("List is empty !!!");
+    void sortCountry() {
+        ArrayList<EastAsiaCountries> sortList = new ArrayList<>();
+
+        sortList.addAll(countryList);
+        if (sortList.isEmpty()) {
+            System.out.println("No Country in the list !!!");
             return;
         }
-        Collection.sort(countryList, new Comparator<EastAsiaCountries>(){
+        Collections.sort(sortList, new Comparator<EastAsiaCountries>() {
             @Override
-            public int compare(EastAsiaCountries o1, EastAsiaCountries o2){
-                return o1.getCountryName().compareToIgnoreCase(o2.countryName);
+            public int compare(EastAsiaCountries o1, EastAsiaCountries o2) {
+                return o1.getCountryName().compareToIgnoreCase(o2.getCountryName());
             }
         });
-    }*/
-    
+        System.out.printf("%-10s %-15s %-10s %-10s\n", "ID", "Name", "Area", "Terrain");
+        for (EastAsiaCountries asiaCountries : sortList) {
+            asiaCountries.display();
+        }
+    }
 }
