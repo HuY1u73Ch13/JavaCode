772431ae4beaa8a1299e169416f1f1b1b5b66a08
Nguyễn Huy Hoàng <jokerhuylu@gmail.com>
2025-05-26 07:21:48 +0700

    Main.java

--- 0052-Countr2/src/Main.java
+++ 0052-Countr2/src/Main.java
@@ -14,9 +14,18 @@ public class Main {
     public static void main(String[] args) {
         Utility u = new Utility();
         ManageEastAsiaCountries manage = new ManageEastAsiaCountries();
+        manage.mockData();
         while (true) {
-            displayMenu();
+            System.out.println("                                   MENU\n" +
+                            "==========================================================================\n" +
+                            "1. Input the information of 11 countries in East Asia\n" +
+                            "2. Display the information of country you've just input\n" +
+                            "3. Search the information of country by user-entered name\n" +
+                            "4. Display the information of countries sorted name in ascending order  \n" +
+                            "5. Exit \n" +
+                            "==========================================================================");
             int option = u.getInteger("Option: ", "Wrong", 1, 5);
+            
             switch (option) {
                 case 1:
                     //input country information
@@ -32,25 +41,12 @@ public class Main {
                     break;
                 case 4:
                     //sort country by code
-                    //manage.sortCountry();
+                    manage.sortCountry();
                     break;
                 case 5:
                     System.exit(0);
                     break;
             }
         }
-        
-
-    }
-
-    private static void displayMenu() {
-        System.out.println("                                   MENU\n" +
-                            "==========================================================================\n" +
-                            "1. Input the information of 11 countries in East Asia\n" +
-                            "2. Display the information of country you've just input\n" +
-                            "3. Search the information of country by user-entered name\n" +
-                            "4. Display the information of countries sorted name in ascending order  \n" +
-                            "5. Exit \n" +
-                            "==========================================================================");
     }
 }
